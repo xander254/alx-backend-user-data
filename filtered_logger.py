7e1561e90fb36faae4180774b9655112894ae549
xander254 <alexkinyanjui356@gmail.com>
2025-05-23 02:38:44 +0300

    Filter personal data using regex

--- filtered_logger.py
+++ filtered_logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-import logging, re
+import logging
+import re
 from typing import List
 """
 A module that returns the log message obfuscated
@@ -7,9 +8,10 @@ A module that returns the log message obfuscated
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
-                    separator: str) -> str:
+                 separator: str) -> str:
     """
     a func that returns the log message obfuscated
     """
-    pattern = r'(' + '|'.join(fields) + r')=.*?(?=' + re.escape(separator) + r')'
+    pattern = r'(' + '|'.join(fields) + r')=.*?(?=' + re.escape(
+            separator) + r')'
     return re.sub(pattern, r'\1=' + redaction, message)
