debbf576a65c655fc1fe0352e71780a253bf88f5
xander254 <alexkinyanjui356@gmail.com>
2025-05-23 03:13:07 +0300

    log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -22,8 +22,8 @@ class RedactingFormatter(logging.Formatter):
 
     def format(self, record: logging.LogRecord) -> str:
         record.msg = filter_datum(self.fields, self.REDACTION,
-                                    record.getMessage(), self.SEPARATOR
-                                    )
+                                  record.getMessage(), self.SEPARATOR
+                                  )
         return super().format(record)
 
 
